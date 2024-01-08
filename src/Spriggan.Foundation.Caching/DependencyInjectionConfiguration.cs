20455927ec88c6080c8e3824e694d5d0d9e57793
André Moraes <andrelom@gmail.com>
2024-01-08 17:59:51 -0300

    Update DependencyInjectionConfiguration.cs

--- src/Spriggan.Foundation.Caching/DependencyInjectionConfiguration.cs
+++ src/Spriggan.Foundation.Caching/DependencyInjectionConfiguration.cs
@@ -12,6 +12,7 @@ public static class DependencyInjectionConfiguration
         // Services
 
         services
+            .AddScoped<ICache, MemoryCache>()
             .AddScoped<IMemoryCache, MemoryCache>();
 
         return services;
