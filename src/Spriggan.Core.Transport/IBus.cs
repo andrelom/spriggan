fff6c664a04421797d81664e627b88bbfba956ff
André Moraes <andrelom@gmail.com>
2024-01-09 18:09:43 -0300

    Create IBus.cs

---
+++ src/Spriggan.Core.Transport/IBus.cs
@@ -0,0 +1,24 @@
+using MassTransit;
+
+namespace Spriggan.Core.Transport;
+
+public interface IBus
+{
+    Task<Response<TResponse>> Request<TRequest, TResponse>(
+        TRequest message,
+        CancellationToken token = default,
+        RequestTimeout timeout = default)
+        where TRequest : class
+        where TResponse : class;
+}
+
+
+public interface ILocalBus : MassTransit.IBus
+{
+    // Intentionally left empty.
+}
+
+public interface IRemoteBus : MassTransit.IBus
+{
+    // Intentionally left empty.
+}
