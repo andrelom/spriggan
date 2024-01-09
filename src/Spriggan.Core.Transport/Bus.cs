19bbfc4b787b6ded8be9ebbfbf0d3828b7e85be6
André Moraes <andrelom@gmail.com>
2024-01-09 18:09:45 -0300

    Update Bus.cs

--- src/Spriggan.Core.Transport/Bus.cs
+++ src/Spriggan.Core.Transport/Bus.cs
@@ -2,11 +2,13 @@ using MassTransit;
 
 namespace Spriggan.Core.Transport;
 
-public class Bus
+public class Bus : IBus
 {
     private readonly ILocalBus _local;
 
     private readonly IRemoteBus _remote;
+
+    private readonly IEnumerable<Type> _types = GetGenericTypeDefinitions();
 
     public Bus(ILocalBus local, IRemoteBus remote)
     {
@@ -14,43 +16,32 @@ public class Bus
         _remote = remote;
     }
 
-    public async Task<Response<TResponse>> Request<TRequest, TResponse>(TRequest message, CancellationToken token = default, RequestTimeout timeout = default) where TRequest : class where TResponse : class
+    public async Task<Response<TResponse>> Request<TRequest, TResponse>(
+        TRequest message,
+        CancellationToken token = default,
+        RequestTimeout timeout = default)
+        where TRequest : class
+        where TResponse : class
     {
-        Response<GetUserResponse> response;
+        var isLocal = _types.Any(type => type == typeof(IConsumer<TRequest>));
 
-        var local = Dependencies.Types.Any(item => item.GetInterfaces().Any(type => type.IsGenericType && type.GetGenericTypeDefinition() == typeof(IConsumer<TRequest>)));
-
-        if (local)
+        if (isLocal)
         {
-            return await _remote.Request<TRequest, TResponse>(message, token, timeout);
+            return await _local.Request<TRequest, TResponse>(message, token, timeout);
         }
 
-        return await _local.Request<TRequest, TResponse>(message, token, timeout);
+        return await _remote.Request<TRequest, TResponse>(message, token, timeout);
     }
 
     #region Private Methods
 
-    // ...?
+    private static IEnumerable<Type> GetGenericTypeDefinitions()
+    {
+        return Dependencies.Types
+            .SelectMany(type => type.GetInterfaces())
+            .Where(type => type.IsGenericType)
+            .Select(type => type.GetGenericTypeDefinition());
+    }
 
     #endregion
-}
-
-public interface ILocalBus : IBus
-{
-    // TODO: ...?
-}
-
-public interface IRemoteBus : IBus
-{
-    // TODO: ...?
-}
-
-public class GetUserRequest
-{
-    public string Name { get; set; }
-}
-
-public class GetUserResponse
-{
-    public string Name { get; set; }
 }
