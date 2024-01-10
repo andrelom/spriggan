6e127510c7f15ca062354e54518952091fca5291
André Moraes <andrelom@gmail.com>
2024-01-10 15:50:47 -0300

    Update ConsumerHostedService.cs

--- src/Spriggan.Core.Transport/ConsumerHostedService.cs
+++ src/Spriggan.Core.Transport/ConsumerHostedService.cs
@@ -1,6 +1,9 @@
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using RabbitMQ.Client;
+using Spriggan.Core.Extensions;
+using Spriggan.Core.Transport.Options;
 
 namespace Spriggan.Core.Transport;
 
@@ -8,11 +11,20 @@ public class ConsumerHostedService : IHostedService
 {
     private readonly ILogger<ConsumerHostedService> _logger;
 
+    private readonly RabbitMqOptions _options;
+
+    private readonly IConnection _connection;
+
+    private readonly IModel _channel;
+
     private readonly IEnumerable<Type> _types = GetRequestHandlerTypes();
 
     public ConsumerHostedService(ILogger<ConsumerHostedService> logger, IConfiguration configuration)
     {
         _logger = logger;
+        _options = configuration.Load<RabbitMqOptions>();
+        _connection = CreateConnection();
+        _channel = _connection.CreateModel();
     }
 
     public Task StartAsync(CancellationToken cancellationToken)
@@ -22,10 +34,25 @@ public class ConsumerHostedService : IHostedService
 
     public Task StopAsync(CancellationToken cancellationToken)
     {
+        _channel.Close();
+        _connection.Close();
+
         return Task.CompletedTask;
     }
 
     #region Private Methods
+
+    private IConnection CreateConnection()
+    {
+        var factory = new ConnectionFactory
+        {
+            HostName = _options.Host,
+            UserName = _options.User,
+            Password = _options.Password,
+        };
+
+        return factory.CreateConnection();
+    }
 
     private static IEnumerable<Type> GetRequestHandlerTypes()
     {
