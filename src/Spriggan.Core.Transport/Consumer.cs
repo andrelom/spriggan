80587bfdabce546a42bd808f2ff26791de063d8a
André Moraes <andrelom@gmail.com>
2024-01-10 16:57:25 -0300

    Update Consumer.cs

--- src/Spriggan.Core.Transport/Consumer.cs
+++ src/Spriggan.Core.Transport/Consumer.cs
@@ -2,6 +2,7 @@ using System.Text.RegularExpressions;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using RabbitMQ.Client;
 using Spriggan.Core.Extensions;
 using Spriggan.Core.Transport.Options;
 
@@ -13,22 +14,33 @@ public class Consumer : IHostedService
 
     private readonly RabbitMqOptions _options;
 
+    private readonly IConnection _connection;
+
+    private readonly IModel _channel;
+
     private readonly IEnumerable<string> _names = GetQueueNames();
 
     public Consumer(ILogger<Consumer> logger, IConfiguration configuration)
     {
+        var connection =  CreateConnection();
+
         _logger = logger;
         _options = configuration.Load<RabbitMqOptions>();
+        _connection = connection;
+        _channel = connection.CreateModel();
     }
 
     public Task StartAsync(CancellationToken cancellationToken)
     {
-        throw new NotImplementedException();
+        return Task.CompletedTask;
     }
 
     public Task StopAsync(CancellationToken cancellationToken)
     {
-        throw new NotImplementedException();
+        _channel.Close();
+        _connection.Close();
+
+        return Task.CompletedTask;
     }
 
     #region Private Methods
@@ -58,6 +70,18 @@ public class Consumer : IHostedService
         {
             return type.GetInterfaces().Any(source => source.IsGenericType && source.GetGenericTypeDefinition() == target);
         });
+    }
+
+    private IConnection CreateConnection()
+    {
+        var factory = new ConnectionFactory
+        {
+            HostName = _options.Host,
+            UserName = _options.User,
+            Password = _options.Password,
+        };
+
+        return factory.CreateConnection();
     }
 
     #endregion
