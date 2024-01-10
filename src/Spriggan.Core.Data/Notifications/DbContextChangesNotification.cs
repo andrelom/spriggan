da50b0d2db47baf8e7a8c38768f6187ff7e80236
André Moraes <andrelom@gmail.com>
2024-01-10 00:42:04 -0300

    Delete DbContextChangesNotification.cs

--- src/Spriggan.Core.Data/Notifications/DbContextChangesNotification.cs
+++
@@ -1,8 +0,0 @@
-using Spriggan.Core.Transport;
-
-namespace Spriggan.Core.Data.Notifications;
-
-public class DbContextChangesNotification : INotification
-{
-    public IEnumerable<KeyValuePair<Guid, Type>> Updates { get; set; } = Array.Empty<KeyValuePair<Guid, Type>>();
-}
