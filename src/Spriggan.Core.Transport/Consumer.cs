da6f3ed8c055ce6e176fca06f427cefa9ff474ca
André Moraes <andrelom@gmail.com>
2024-01-10 16:49:57 -0300

    Update Consumer.cs

--- src/Spriggan.Core.Transport/Consumer.cs
+++ src/Spriggan.Core.Transport/Consumer.cs
@@ -1,9 +1,23 @@
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Spriggan.Core.Extensions;
+using Spriggan.Core.Transport.Options;
 
 namespace Spriggan.Core.Transport;
 
 public class Consumer : IHostedService
 {
+    private readonly ILogger<Consumer> _logger;
+
+    private readonly RabbitMqOptions _options;
+
+    public Consumer(ILogger<Consumer> logger, IConfiguration configuration)
+    {
+        _logger = logger;
+        _options = configuration.Load<RabbitMqOptions>();
+    }
+
     public Task StartAsync(CancellationToken cancellationToken)
     {
         throw new NotImplementedException();
