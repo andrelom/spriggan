61176832d227114e7c15178dbfea632265f2d7c9
André Moraes <andrelom@gmail.com>
2024-01-16 15:38:42 -0300

    Update RabbitMqConsumer.cs

--- src/Spriggan.Core.Transport/RabbitMqConsumer.cs
+++ src/Spriggan.Core.Transport/RabbitMqConsumer.cs
@@ -1,65 +1,16 @@
-using EasyNetQ;
 using Microsoft.Extensions.Hosting;
 
 namespace Spriggan.Core.Transport;
 
 public class RabbitMqConsumer : IHostedService
 {
-    private readonly EasyNetQ.IBus _bus;
-
-    public RabbitMqConsumer()
+    public Task StartAsync(CancellationToken cancellationToken)
     {
-        _bus = RabbitHutch.CreateBus("host=localhost");
-    }
-
-    public async Task StartAsync(CancellationToken cancellationToken)
-    {
-        await BindRequestHandlers();
-        await BindSubscribeHandlers();
+        return Task.CompletedTask;
     }
 
     public Task StopAsync(CancellationToken cancellationToken)
     {
         return Task.CompletedTask;
     }
-
-    #region Private Methods
-
-    private static IEnumerable<Type> GetRequestTypes()
-    {
-        var target = typeof(IRequest<>);
-
-        return Dependencies.Domain.Where(type =>
-        {
-            return type.GetInterfaces().Any(source => source.IsGenericType && source.GetGenericTypeDefinition() == target);
-        });
-    }
-
-    private async Task BindRequestHandlers()
-    {
-        var types = GetRequestTypes();
-        var source = _bus.PubSub.GetType().GetMethod(nameof(_bus.PubSub.SubscribeAsync));
-
-        if (source == null) return;
-
-        foreach (var type in types)
-        {
-            var method = source.MakeGenericMethod(type);
-            var action = typeof(Action<>).MakeGenericType(type);
-
-            var activator = Activator.CreateInstance(action, new Action<object>(response =>
-            {
-                Console.WriteLine(response);
-            }));
-
-            method.Invoke(_bus.PubSub, new[] { "id", activator });
-        }
-    }
-
-    private Task BindSubscribeHandlers()
-    {
-        return Task.CompletedTask;
-    }
-
-    #endregion
 }
