56a5543b027272288bb3aa0c304d47c3d5b6b7a3
André Moraes <andrelom@gmail.com>
2024-01-16 15:42:03 -0300

    Update RabbitMqConsumer.cs

--- src/Spriggan.Core.Transport/RabbitMqConsumer.cs
+++ src/Spriggan.Core.Transport/RabbitMqConsumer.cs
@@ -4,6 +4,10 @@ namespace Spriggan.Core.Transport;
 
 public class RabbitMqConsumer : IHostedService
 {
+    private static readonly IEnumerable<Type> Requests = GetRequestTypes();
+
+    private static readonly IEnumerable<Type> Notifications = GetNotificationTypes();
+
     public Task StartAsync(CancellationToken cancellationToken)
     {
         return Task.CompletedTask;
@@ -13,4 +17,28 @@ public class RabbitMqConsumer : IHostedService
     {
         return Task.CompletedTask;
     }
+
+    #region Private Methods
+
+    private static IEnumerable<Type> GetRequestTypes()
+    {
+        var target = typeof(IRequest<>);
+
+        return Dependencies.Domain.Where(type =>
+        {
+            return type.GetInterfaces().Any(source => source.IsGenericType && source.GetGenericTypeDefinition() == target);
+        });
+    }
+
+    private static IEnumerable<Type> GetNotificationTypes()
+    {
+        var target = typeof(INotification);
+
+        return Dependencies.Domain.Where(type =>
+        {
+            return type.GetInterfaces().Any(source => source.IsGenericType && source.GetGenericTypeDefinition() == target);
+        });
+    }
+
+    #endregion
 }
