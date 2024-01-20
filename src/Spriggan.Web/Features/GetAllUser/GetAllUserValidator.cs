f5b5cd7490fce757c4b0c94981751e332434498f
André Moraes <andrelom@gmail.com>
2024-01-20 01:04:36 -0300

    Create GetAllUserValidator.cs

---
+++ src/Spriggan.Web/Features/GetAllUser/GetAllUserValidator.cs
@@ -0,0 +1,11 @@
+using FastEndpoints;
+
+namespace Spriggan.Web.Features.GetAllUser;
+
+public class GetAllUserValidator: Validator<GetAllUserRequest>
+{
+    public GetAllUserValidator()
+    {
+        // Intentionally left empty.
+    }
+}
