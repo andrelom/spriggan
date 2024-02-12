f3d642e1f4edea73c96284ee3ce711bbdc821992
André Moraes <andrelom@gmail.com>
2024-02-12 14:27:38 -0300

    Update IRequestHandler.cs

--- src/Spriggan.Core.Transport/IRequestHandler.cs
+++ src/Spriggan.Core.Transport/IRequestHandler.cs
@@ -1,8 +1,8 @@
 namespace Spriggan.Core.Transport;
 
 public interface IRequestHandler<in TRequest, TResponse> : MediatR.IRequestHandler<TRequest, TResponse>
-    where TRequest : class, IRequest<TResponse>
-    where TResponse : class
+    where TRequest : IRequest<TResponse>
+    where TResponse : IResult
 {
     // Intentionally left empty.
 }
