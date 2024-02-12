81d16449afd0406edcbb389f8bfe4adf2e2823ae
André Moraes <andrelom@gmail.com>
2024-02-12 10:40:24 -0300

    Update IMiddleware.cs

--- src/Spriggan.Core.Transport/IMiddleware.cs
+++ src/Spriggan.Core.Transport/IMiddleware.cs
@@ -1,10 +1,8 @@
 namespace Spriggan.Core.Transport;
 
-public delegate Task<TResponse> RequestDelegate<TRequest, TResponse>(TRequest request) where TRequest : class, IRequest<TResponse> where TResponse : class;
+public delegate Task<TResponse> RequestDelegate<in TRequest, TResponse>(TRequest request) where TRequest : class, IRequest<TResponse> where TResponse : class;
 
-public interface IMiddleware<TRequest, TResponse>
-    where TRequest : class, IRequest<TResponse>
-    where TResponse : class
+public interface IMiddleware<TRequest, TResponse> where TRequest : class, IRequest<TResponse> where TResponse : class
 {
     Task<TResponse> Handle(TRequest request, RequestDelegate<TRequest, TResponse> next, CancellationToken cancel);
 }
