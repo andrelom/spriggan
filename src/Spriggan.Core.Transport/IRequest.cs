0be3bdb70f6d55da8259de3d6f8ec3d721809ef5
André Moraes <andrelom@gmail.com>
2024-02-12 14:27:32 -0300

    Update IRequest.cs

--- src/Spriggan.Core.Transport/IRequest.cs
+++ src/Spriggan.Core.Transport/IRequest.cs
@@ -1,6 +1,6 @@
 namespace Spriggan.Core.Transport;
 
-public interface IRequest<out TResponse> : IMessage, MediatR.IRequest<TResponse> where TResponse : class
+public interface IRequest<out TResponse> : IMessage, MediatR.IRequest<TResponse> where TResponse : IResult
 {
     // Intentionally left empty.
 }
