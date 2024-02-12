00a64704ff478fdfff536de814521f40f0e8e773
André Moraes <andrelom@gmail.com>
2024-02-12 14:27:34 -0300

    Update IMediator.cs

--- src/Spriggan.Core.Transport/IMediator.cs
+++ src/Spriggan.Core.Transport/IMediator.cs
@@ -2,7 +2,13 @@ namespace Spriggan.Core.Transport;
 
 public interface IMediator
 {
-    Task<TResponse> Send<TResponse>(IRequest<TResponse> request, CancellationToken cancel = default) where TResponse : class;
+    Task<TResponse> Send<TResponse>(
+        IRequest<TResponse> request,
+        CancellationToken cancel = default)
+        where TResponse : IResult;
 
-    Task Publish<TNotification>(TNotification notification, CancellationToken cancel = default) where TNotification : INotification;
+    Task Publish<TNotification>(
+        TNotification notification,
+        CancellationToken cancel = default)
+        where TNotification : INotification;
 }
