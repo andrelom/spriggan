d33e8331ba00c9485a2b04ba0b50644c881adfef
André Moraes <andrelom@gmail.com>
2024-02-13 14:38:33 -0300

    Update INotificationHandler.cs

--- src/Spriggan.Core.Transport/INotificationHandler.cs
+++ src/Spriggan.Core.Transport/INotificationHandler.cs
@@ -1,6 +1,8 @@
 namespace Spriggan.Core.Transport;
 
-public interface INotificationHandler<in TNotification> : MediatR.INotificationHandler<TNotification> where TNotification : INotification
+public interface INotificationHandler<in TNotification> :
+    MediatR.INotificationHandler<TNotification>
+    where TNotification : INotification
 {
     // Intentionally left empty.
 }
