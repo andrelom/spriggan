a6f0a330a243e5bcf25a07fcdb4aaf30611ce148
André Moraes <andrelom@gmail.com>
2024-02-13 00:12:30 -0300

    Update MassTransitConsumer.cs

--- src/Spriggan.Core.Transport/Extensions/MassTransitExtensions.cs
+++ src/Spriggan.Core.Transport/Extensions/MassTransitExtensions.cs
@@ -1,4 +1,5 @@
 using MassTransit;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace Spriggan.Core.Transport.Extensions;
 
@@ -6,6 +7,19 @@ public static class MassTransitExtensions
 {
     public static void AddMassTransitConsumers(this IBusRegistrationConfigurator configurator)
     {
-        // TODO: ...?
+        var massTransitConsumerGenericType = typeof(MassTransitConsumer<,>);
+        var consumerGenericType = typeof(IConsumer<,>);
+        var consumerTypes = Dependencies.Domain.Where(item => item.GetInterfaces().Any(type => type.IsGenericType && type.GetGenericTypeDefinition() == consumerGenericType));
+
+        foreach (var consumerType in consumerTypes)
+        {
+            var closedConsumerGenericType = consumerType.GetInterfaces().First(type => type.IsGenericType && type.GetGenericTypeDefinition() == consumerGenericType);
+
+            configurator.AddScoped(closedConsumerGenericType, consumerType);
+
+            var closedMassTransitConsumerGenericType = massTransitConsumerGenericType.MakeGenericType(closedConsumerGenericType.GetGenericArguments());
+
+            configurator.AddConsumer(closedMassTransitConsumerGenericType);
+        }
     }
 }
