1e76e64356f74205015f43ad506bd752fd44f020
André Moraes <andrelom@gmail.com>
2024-02-13 13:14:48 -0300

    Update GetAllUserRequest.cs

--- src/Spriggan.Web/Features/GetAllUser/GetAllUserRequest.cs
+++ src/Spriggan.Web/Features/GetAllUser/GetAllUserRequest.cs
@@ -1,6 +1,9 @@
+using FastEndpoints;
+using Spriggan.Core;
+
 namespace Spriggan.Web.Features.GetAllUser;
 
-public class GetAllUserRequest
+public class GetAllUserRequest : ICommand<IResult<GetAllUserResponse>>
 {
     // Intentionally left empty.
 }
