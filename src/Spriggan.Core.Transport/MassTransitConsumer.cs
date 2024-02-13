58dac0c81e498f90875948506966e24b190945d9
André Moraes <andrelom@gmail.com>
2024-02-12 22:29:18 -0300

    Create MassTransitConsumer.cs

---
+++ src/Spriggan.Core.Transport/MassTransitConsumer.cs
@@ -0,0 +1,21 @@
+using MassTransit;
+
+namespace Spriggan.Core.Transport;
+
+public class MassTransitConsumer<TRequest, TResponse> :
+    IConsumer<TRequest>
+    where TRequest : class, IRequest<TResponse>
+    where TResponse : class, IResult
+{
+    private readonly IConsumer<TRequest, TResponse> _consumer;
+
+    public MassTransitConsumer(IConsumer<TRequest, TResponse> consumer)
+    {
+        _consumer = consumer;
+    }
+
+    public async Task Consume(ConsumeContext<TRequest> context)
+    {
+        await _consumer.Consume(context.Message);
+    }
+}
