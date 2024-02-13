282702ccf38aad21d78591571f8aad890eeeb813
André Moraes <andrelom@gmail.com>
2024-02-12 22:28:35 -0300

    Update IConsumer.cs

--- src/Spriggan.Core.Transport/IConsumer.cs
+++ src/Spriggan.Core.Transport/IConsumer.cs
@@ -1,8 +1,6 @@
 namespace Spriggan.Core.Transport;
 
-public interface IConsumer<in TRequest, TResponse>
-    where TRequest : IRequest<TResponse>
-    where TResponse : IResult
+public interface IConsumer<in TRequest, TResponse> where TRequest : IRequest<TResponse> where TResponse : IResult
 {
     Task<TResponse> Consume(TRequest message);
 }
