f3f3ba9942982ee7546ddff465b1b0e05d3e1b7f
André Moraes <andrelom@gmail.com>
2024-02-12 23:55:09 -0300

    Create MassTransitExtensions.cs

---
+++ src/Spriggan.Core.Transport/Extensions/MassTransitExtensions.cs
@@ -0,0 +1,11 @@
+using MassTransit;
+
+namespace Spriggan.Core.Transport.Extensions;
+
+public static class MassTransitExtensions
+{
+    public static void AddMassTransitConsumers(this IBusRegistrationConfigurator configurator)
+    {
+        // TODO: ...?
+    }
+}
