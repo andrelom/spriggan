821494b12e12bd90ead7e3e6a192fccc94c53043
André Moraes <andrelom@gmail.com>
2024-02-14 09:18:07 -0300

    Use controllers

--- src/Spriggan.Module.Identity/Controllers/UsersController.cs
+++ src/Spriggan.Module.Identity/Controllers/UsersController.cs
@@ -1,23 +1,35 @@
-using MediatR;
-using Microsoft.AspNetCore.Http;
+﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Spriggan.Core;
+using Spriggan.Core.Transport;
 using Spriggan.Data.Identity.Contracts.Values;
 using Spriggan.Foundation.Identity.Attributes;
+using Spriggan.Module.Identity.Contracts.Features.GetAllUser;
 using Spriggan.Module.Identity.Contracts.Features.GetUserByName;
-using ControllerBase = Spriggan.Core.Web.ControllerBase;
 
-namespace Spriggan.Module.Identity.Features.GetUserByName;
+namespace Spriggan.Module.Identity.Controllers;
 
 [Tags("Identity: Users")]
-[Route("/identity/users")]
-public class GetUserByNameController : ControllerBase
+public class UsersController : ControllerBase
 {
     private readonly IMediator _mediator;
 
-    public GetUserByNameController(IMediator mediator)
+    public UsersController(IMediator mediator)
     {
         _mediator = mediator;
+    }
+
+    [UserRoles(Roles.Administrator.Name)]
+    [HttpGet]
+    [Route("")]
+    [ProducesResponseType(typeof(Result<GetAllUserResponse>), StatusCodes.Status200OK)]
+    public async Task<IActionResult> GetAll([FromQuery] GetAllUserRequest request)
+    {
+        var response = await _mediator.Send(request);
+
+        return response.Ok
+            ? Ok(response)
+            : StatusCode(StatusCodes.Status400BadRequest, response);
     }
 
     [UserRoles(Roles.Administrator.Name)]
