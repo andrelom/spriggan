da1348e77730ffa0ff934401a140279fd92a9b4e
André Moraes <andrelom@gmail.com>
2024-02-14 13:17:09 -0300

    Update AccountController.cs

--- src/Spriggan.Module.Identity/Controllers/AccountController.cs
+++ src/Spriggan.Module.Identity/Controllers/AccountController.cs
@@ -10,6 +10,7 @@ using Spriggan.Module.Identity.Contracts.Features.ResetPassword;
 using Spriggan.Module.Identity.Contracts.Features.SendResetPasswordToken;
 using Spriggan.Module.Identity.Contracts.Features.SignIn;
 using Spriggan.Module.Identity.Contracts.Features.SignUp;
+using ControllerBase = Spriggan.Core.Web.ControllerBase;
 
 namespace Spriggan.Module.Identity.Controllers;
 
