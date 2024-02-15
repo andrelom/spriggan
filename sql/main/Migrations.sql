1f74ff53aac7b1378b21cc09ac340d3f44143363
André Moraes <andrelom@gmail.com>
2024-02-14 22:34:15 -0300

    Create Migrations.sql

---
+++ sql/main/Migrations.sql
@@ -0,0 +1,18 @@
+﻿CREATE TABLE IF NOT EXISTS "__EFMigrationsHistory" (
+    "MigrationId" character varying(150) NOT NULL,
+    "ProductVersion" character varying(32) NOT NULL,
+    CONSTRAINT "PK___EFMigrationsHistory" PRIMARY KEY ("MigrationId")
+);
+
+START TRANSACTION;
+
+
+DO $EF$
+BEGIN
+    IF NOT EXISTS(SELECT 1 FROM "__EFMigrationsHistory" WHERE "MigrationId" = '20240215013158_v1.0.0') THEN
+    INSERT INTO "__EFMigrationsHistory" ("MigrationId", "ProductVersion")
+    VALUES ('20240215013158_v1.0.0', '8.0.2');
+    END IF;
+END $EF$;
+COMMIT;
+
