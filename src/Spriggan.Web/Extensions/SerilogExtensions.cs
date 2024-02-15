e06aa345cabbb733a0f8393cfdb41ed8e950a6aa
André Moraes <andrelom@gmail.com>
2024-02-14 22:00:43 -0300

    Update SerilogExtensions.cs

--- src/Spriggan.Web/Extensions/SerilogExtensions.cs
+++ src/Spriggan.Web/Extensions/SerilogExtensions.cs
@@ -7,7 +7,7 @@ public static class SerilogExtensions
 {
     public static void AddSerilog(this IServiceCollection services)
     {
-        const string path = "./tmp/logs/{Date}.txt";
+        const string path = "./tmp/logs/{Date}.log";
         const string template = "{Timestamp:yyyy-MM-dd HH:mm:ss} [{Level:u3}] {Message:lj}{NewLine}{Exception}";
 
         Log.Logger = new LoggerConfiguration()
